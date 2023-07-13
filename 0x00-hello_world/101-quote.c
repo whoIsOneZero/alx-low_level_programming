e545ba5b0846a391a0e509ceb2aadcac99468d8b
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-07-13 13:09:06 -0700

    doc: prints using fprintf

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 1 (success)
+ */
+int main(void)
+{
+	fprintf(stdout, "and that piece of art is useful\" -
+			Dora Korpar, 2015-10-19");
+
+	return (1);
+}
