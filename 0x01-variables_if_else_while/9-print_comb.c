be2c1447b4d79b96ea3fe60bde2a992eba417e96
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-07-14 18:08:49 -0700

    doc: prints all single nums. with COMMA and SPACE

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,12 +10,15 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i <= 56; i++)
+	for (i = 48; i <= 57; i++)
 	{
 		putchar(i);
-		putchar(44);
-		putchar(32);
+		if (i < 57) 
+		{
+			putchar(44);
+			putchar(32);
+		}
 	}
-	putchar(57);
+	putchar('\n');
 	return (0);
 }
