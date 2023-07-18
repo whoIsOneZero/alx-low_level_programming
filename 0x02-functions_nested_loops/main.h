1cad2e89b22b9efd93445020d63a788d705d1c82
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-07-18 16:07:19 -0700

    doc: Made final changes, hopefully

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,15 +1,1 @@
-#ifndef PRINT_MACRO
-#define PRINT_MACRO
-
-#include <stdio.h>
-/**
- * _putchar - prints a character
- *
- * Return: void always
- * @print_this: character array
- */
-void _putchar(char print_this[])
-{
-	printf("%s\n", print_this);
-}
-#endif
+void _putchar(char print_this[]);
