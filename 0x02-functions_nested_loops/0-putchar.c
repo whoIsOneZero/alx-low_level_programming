e6c2800f172c2669556ffb004fc577bd3874c375
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-07-18 14:02:53 -0700

    doc: prints something, using a header file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
-#include<stdio.h>
+#include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry point
@@ -8,6 +9,6 @@
 
 int main(void)
 {
-	printf("_putchar\n");
+	print_this();
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef PRINT_MACRO
+#define PRINT_MACRO
+
+#include <stdio.h>
+/**
+ * print_this - prints a character
+ *
+ * Return: void always
+ */
+void print_this(void)
+{
+	printf("_putchar\n");
+}
+#endif
