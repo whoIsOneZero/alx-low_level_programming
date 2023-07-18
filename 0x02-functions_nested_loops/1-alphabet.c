30a1f65c27fec21b17be32151420ff6907f66f4e
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-07-18 16:44:21 -0700

    doc: prints lowercase alphabets: fxn def. in file specified

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,6 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - entry point
- *
- * Return: always 0 (success)
- */
-
-int main(void)
-{
-	print_alphabet();
-}
-
-/**
  * print_alphabet - prints alphabets
  *
  * Return: always 0 (success)
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+        print_alphabet();
+	return (0);
+}
