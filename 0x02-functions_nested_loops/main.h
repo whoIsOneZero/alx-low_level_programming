06471901a133ea8e9ca3ef9c0e00d3b2627a0b60
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-07-18 15:30:27 -0700

    Revert "doc: prints something, using a header file"
    This reverts commit e6c2800f172c2669556ffb004fc577bd3874c375.

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,14 @@
 #ifndef PRINT_MACRO
 #define PRINT_MACRO
-void _putchar(char to_print[]);
+
+#include <stdio.h>
+/**
+ * print_this - prints a character
+ *
+ * Return: void always
+ */
+void _putchar(char print_this[])
+{
+	printf("%s\n", print_this);
+}
 #endif
