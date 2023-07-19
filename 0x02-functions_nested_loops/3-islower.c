a6df182f1fba095ac830ea1fbd1a74d45a204fb3
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-07-18 17:51:43 -0700

    feat: checks for lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,10 +6,12 @@
  * _islower - checks for lowercase character
  *
  * Return: 0 (not lowercase), any other int otherwise
+ * @c: item to be checked
  */
 
 int _islower(int c)
 {
 	int ans = islower(c);
+
 	return (ans);
 }
--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -3,18 +3,17 @@
 /**
  * main - entry point.
  *
- * Return: Always 0.
+ * Return: Always 0
  */
+
 int main(void)
 {
-    int r;
-
-    r = _islower('H');
-    _putchar(r + '0');
-    r = _islower('o');
-    _putchar(r + '0');
-    r = _islower(108);
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
+	r = _islower('H);
+	_putchar(r + '0');
+	r = islwer('o');
+	_putchar(r + '0');
+	r = _islower(108);
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);
 }
