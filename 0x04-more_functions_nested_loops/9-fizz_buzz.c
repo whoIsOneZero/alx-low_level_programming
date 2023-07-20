9ef231e67d7ebfc08c2253e2d60bf2209d5c69dc
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-07-20 16:24:26 -0700

    doc: The Fizz-Buzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,18 +12,22 @@ int main(void)
 	{
 		if ((i > 14) && ((i % 3 == 0) && (i % 5 == 0)))
 		{
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		}
 		else if ((i > 2) && ((i % 3 == 0 && (i % 5) != 0)))
 		{
-			printf("Fizz ");
+			printf(" Fizz ");
 		}
 		else if ((i > 4) && ((i % 5 == 0) && (i % 3 != 0)))
 		{
-			printf("Buzz ");
+			printf(" Buzz ");
+		}
+		else if (i == 1)
+		{
+			printf("%d", i);
 		}
 		else
-		printf("%d ", i);
+		printf(" %d", i);
 	}
 	printf("\n");
 	return (0);
