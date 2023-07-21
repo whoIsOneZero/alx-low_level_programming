cc15ce60e9931ab565a8bdb9b0bb74da1e4ec897
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-07-20 17:01:11 -0700

    doc: prints a triangle in the terminal

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -17,9 +17,9 @@ void print_triangle(int size)
 
 		for (a = 1; a <= size; a++)
 		{
-			for (b = a; b <= size; b++)
+			for (b = a; b < size; b++)
 			{
-				_putchar(32);
+				_putchar(' ');
 			}
 			for (b = 1; b <= a; b++)
 			{
