1d0c3bdad7797e6c7bbe207b9feb5d0284b4595e
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-07-24 12:58:46 -0700

    doc: returns the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include <string.h>
+/**
+ * _strlen - returns the length of a string
+ * @s: the paramenter
+ *
+ * Return: string length (int)
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	length = strlen(s);
+	return (length);
+}
