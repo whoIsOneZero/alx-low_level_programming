6d9fbd3a39fb03496dfbbb13463dc4c70d518894
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-07-24 18:22:01 -0700

    doc: protype of strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include <string.h>
+
+/**
+ * _strcpy - prototype of strcpy
+ * @dest: where the string will be copied to.
+ * @src: the source
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i, length;
+
+	length = strlen(src);
+	for (i = 0; i < length && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < length; i++)
+		dest[i] = '\0';
+	return (dest);
+}
