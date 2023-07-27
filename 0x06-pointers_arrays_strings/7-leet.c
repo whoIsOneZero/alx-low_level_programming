a28ad410ab72ad41910b8f525b7b712f73f035c8
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-07-26 18:56:54 -0700

    doc: encodes a string to 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+/**
+ * leet - encodes a string into 1337
+ * @s: the string to be encoded
+ * Return: a pointer to the encoded string
+ */
+char *leet(char *s)
+{
+	int i, j;
+
+	char check[] = "aAeEoOtTlL";
+	char rep[] = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if(s[i] == check[j])
+			{
+				s[i] = rep[j];
+			}
+		}
+	}
+	return (s);
+}
