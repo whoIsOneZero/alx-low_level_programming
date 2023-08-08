a6ac510de01a3c5be4d73a99019d1233c8d2d54b
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-08-08 03:41:20 -0700

    doc: creates a new space in memory which contains a copy of the string given as a parameter.

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,32 +10,30 @@
  */
 char *_strdup(char *str)
 {
-	unsigned int i, size = 0;
+	unsigned int i, size;
 	char *p;
-	/*Get size of str*/
 
+	i = 0;
+	size = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	/*Get size of str*/
 	while (str[size])
 	{
 		size++;
 	}
 
-	if (size == 0)
-		return (NULL);
-
-	p = (char *) malloc(sizeof(char) * (size + 1));
+	p =  malloc(sizeof(char) * (size + 1));
 	/*size + 1 for the null terminating character*/
 
-	/*returns NUL on memory allocation fail*/
+	/*returns NULL on memory allocation fail*/
 	if (p == NULL)
 		return (NULL);
 
-	else
-	{
-		for (i = 0; i < size; i++)
-		{
-			*(p + i) = *(str + i);
-		}
-		*(p + i) = '\0';
-	}
+	while ((p[i] = str[i]) != '\0')
+			i++;
+
 	return (p);
 }
