5e8a2b9d44156953a16631c58e5385e4c607c6c2
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-08-11 11:57:58 -0700

    doc: allocates memory using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: memory size in bytes
+ * Return: pointer to allocated memory (success)
+ * or, status value of 98 (fail)
+ */
+void *malloc_checked(unsigned int b)
+{
+	/*a generic pointer that can point to any type of data*/
+	void *p;
+
+	p = malloc(b);
+
+	if(p == NULL)
+		exit(98);
+
+	return(p);
+}
