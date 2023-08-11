6601f36d10eb67e394c792478726579f37c53182
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-08-11 13:15:45 -0700

    doc: tasks to be retaken

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,5 +5,5 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-void _puts_recursion(char *s);
+int _putchar(char data);
 #endif
