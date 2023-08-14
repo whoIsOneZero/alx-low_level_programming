3a93461632c630f6866981e8af278c7638d6511f
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-08-14 14:57:34 -0700

    doc: updated README.md for Repo

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * init_dog - initializes a variable of type "struct dog"
+ * @d: pointer to the struct
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog d;
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # alx-low_level_programming
-Already had a README.
+C Programming.
+Taks covering various concepts in C.
