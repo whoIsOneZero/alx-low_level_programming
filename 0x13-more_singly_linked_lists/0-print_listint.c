c2e7d2fec3e3e0f8776ae1ac409538af044c8c04
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-08-29 14:44:45 -0700

    doc: prints all the elements of a linked list; data - int type

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,12 +11,14 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
+	const listint_t *temp;
 
-	while (h != NULL)
+	temp = h;
+	while (temp != NULL)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
+		printf("%d\n", temp->n);
 		count++;
+		temp = temp->next;
 	}
 
 	return (count);
