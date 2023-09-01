7792b482e1bd0d87bde33f1e6cb7116c55d97695
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-08-31 17:44:21 -0700

    doc: returns the value of a bit at a given index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,18 +2,23 @@
 #include <stdio.h>
 /**
  * get_bit - get the value of a bit at a given index
- * @n: number to evaluate
+ * @n: number to evaluate (decimal)
  * @index: index starting from 0, of the bit we want to get
- * Return: Value of bit at index, or -1 if error
+ * Return: Value of bit at index (SUCCESS)
+ * or -1 if (FAIL)
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int hold;
+	unsigned long int this_bit;
 
+	/*sizeof(unsigned long) = 8 bytes*/
+	/*64 bits with index 0 - 63*/
 	if (index > 64)
 		return (-1);
 
-	hold = n >> index;
+	/*Make the desired bit the LSB*/
+	this_bit = n >> index;
 
+	/*All other bits -> 0 except LSB*/
 	return (hold & 1);
 }
